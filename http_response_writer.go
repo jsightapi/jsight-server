082e1d32abc02ddbb64ad55d85981f8a74b7d922
Maxim Reznitskiy <emptyfruit@gmail.com>
2024-05-16 23:29:29 +0400

    .

--- http_response_writer.go
+++ http_response_writer.go
@@ -64,7 +64,7 @@ func (r httpResponseWriter) errorPageReload() {
 	info := errorInfo{
 		"Error",
 		"A new version of JSight Online Editor 6.0 has been released. Please, refresh the page in your browser (Ctrl+Shift+R)",
-		0, 0,
+		1, 1,
 	}
 
 	b, err := json.Marshal(info)
